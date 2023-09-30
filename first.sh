1bebdd800b686087d1185e2714b2df88506d0468
Ferdinand Mom <ferdinand.mom@epita.fr>
2023-09-30 16:26:18 +0000

    correct echo eval pyenv

--- first.sh
+++ first.sh
@@ -1,22 +1,22 @@
 # ========== ssh ==========
 
-ssh-keygen -t ed25519 -C "ferdinand.mom@epita.fr"
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_ed25519
+#ssh-keygen -t ed25519 -C "ferdinand.mom@epita.fr"
+#eval "$(ssh-agent -s)"
+#ssh-add ~/.ssh/id_ed25519
 
-git config --global user.email "ferdinand.mom@epita.fr"
-git config --global user.name "Ferdinand Mom"
+#git config --global user.email "ferdinand.mom@epita.fr"
+#git config --global user.name "Ferdinand Mom"
 
 # ========== Bash ==========
 # ls autocomplete
-echo "bind 'set show-all-if-ambiguous on'" >> ~/.bashrc
-echo "bind 'TAB:menu-complete'" >> ~/.bashrc
+#echo "bind 'set show-all-if-ambiguous on'" >> ~/.bashrc
+#echo "bind 'TAB:menu-complete'" >> ~/.bashrc
 
 # Fuzzer
-echo "# Fuzzer" >>  ~/.bashrc
-echo "[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh" >> ~/.bashrc
+#echo "# Fuzzer" >>  ~/.bashrc
+#echo "[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh" >> ~/.bashrc
 
 # Pyenv
-export PYENV_ROOT="$HOME/.pyenv"
-echo "command -v pyenv >/dev/null || export PATH=\"$PYENV_ROOT/bin:$PATH\"" >> ~/.bashrc
-echo "eval \"$(pyenv init -)\"" >> ~/.bashrc
+#export PYENV_ROOT="$HOME/.pyenv"
+#echo "command -v pyenv >/dev/null || export PATH=\"$PYENV_ROOT/bin:$PATH\"" >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
