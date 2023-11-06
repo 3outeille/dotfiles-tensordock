16aec9c3ec79b7d0e0a07be6f3712e1de699c097
Ferdinand Mom <ferdinand.mom@epita.fr>
2023-11-06 21:03:30 +0000

    add automatic yes to all files

--- first.sh
+++ first.sh
@@ -1,17 +1,17 @@
 # ========== ssh ==========
 
-ssh-keygen -t ed25519 -C "ferdinand.mom@epita.fr"
+yes | ssh-keygen -t ed25519 -C "ferdinand.mom@epita.fr"
 eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_ed25519
+yes | ssh-add ~/.ssh/id_ed25519
 
 git config --global user.email "ferdinand.mom@epita.fr"
 git config --global user.name "Ferdinand Mom"
 git config --global core.editor "code --wait"
 
 # ========== ZSH ==========
-sudo apt install zsh
+yes | sudo apt install zsh
 # install oh-my-zsh
-sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+yes | sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
 # Fuzzer
 echo "# Fuzzer" >>  ~/.zshrc
--- second.sh
+++ second.sh
@@ -8,18 +8,18 @@ mkdir -p $HOME/.zsh
 ln -s $HOME/dotfiles-tensordock/config/aliases.zsh $HOME/.zsh/aliases.zsh
 
 # Change shell to zsh
-sudo chsh -s $(which zsh)
+yes | sudo chsh -s $(which zsh)
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 mv zsh-syntax-highlighting ${HOME}/.oh-my-zsh/plugins
 
 
 # ===== Vim =====
-sudo apt install vim
+yes | sudo apt install vim
 
 # ====== Fuzzer Finder =====
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+yes | ~/.fzf/install
 
 # ======= Pdbpp =========
 rm $HOME/.pdbrc.py
@@ -30,15 +30,15 @@ ln -s $HOME/dotfiles-tensordock/config/pdbrc $HOME/.pdbrc
 
 # ======== Nvtop ======
 # https://github.com/Syllo/nvtop/issues/51#issuecomment-759600674
-sudo apt install libdrm-dev libsystemd-dev
-sudo snap install cmake --classic
-sudo ln -s /snap/bin/cmake /usr/bin/cmake
-sudo apt install libncurses5-dev libncursesw5-dev git
+yes | sudo apt install libdrm-dev libsystemd-dev
+yes | sudo snap install cmake --classic
+yes | sudo ln -s /snap/bin/cmake /usr/bin/cmake
+yes | sudo apt install libncurses5-dev libncursesw5-dev git
 git clone https://github.com/Syllo/nvtop.git
 mkdir -p nvtop/build && cd nvtop/build
 cmake .. -DNVIDIA_SUPPORT=ON -DAMDGPU_SUPPORT=ON -DINTEL_SUPPORT=ON
 make
-sudo make install
+yes | sudo make install
 
 cd ~
 
